744e0807b0e7c1b6c038dce75fc5371bdf95ee7c
Sasha Larson <sjlarson92@gmail.com>
2020-02-22 12:35:17 -0600

    removed if statement for prompt render, this fixes the next/previous button bug

--- src/store/dispatchFunctions.js
+++ src/store/dispatchFunctions.js
@@ -5,26 +5,22 @@ export const updateNextDateAction = () => (dispatch, getState) => {
   const nextDate = moment(getState().date)
     .add(1, "day")
     .format("YYYY-MM-DD");
-  if (getState().prompts[nextDate] !== undefined) {
     dispatch({
       type: TYPES.UPDATE_DATE,
       payload: {
         date: nextDate
       }
     });
-  }
 };
 
 export const updatePreviousDateAction = () => (dispatch, getState) => {
   const previousDate = moment(getState().date)
     .subtract(1, "day")
     .format("YYYY-MM-DD");
-  if (getState().prompts[previousDate] !== undefined) {
     dispatch({
       type: TYPES.UPDATE_DATE,
       payload: {
         date: previousDate
       }
     });
-  }
 };
