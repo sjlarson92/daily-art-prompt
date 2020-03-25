0de6247fd279dc87b60839b5ac8724647e854db1
Sasha Larson <sjlarson92@gmail.com>
2020-03-24 20:17:25 -0500

    changed headers to auth to pass user info

--- src/main/User/Login/authRequests.js
+++ src/main/User/Login/authRequests.js
@@ -4,8 +4,8 @@ export const validateLogin = (dispatch, history, email, password) => {
   console.log('email: ', email)
   const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL
   axios.post(`${GATEWAY_URL}/api/login`, null, {
-    headers: {
-      email,
+    auth: {
+      username: email,
       password,
     },
   })
