6b44f0f471eeacc4315258d9d25c8be59fad2b7d
Sasha Larson <sjlarson92@gmail.com>
2020-04-20 18:00:22 -0500

    removed console.logs

--- src/main/User/Login/authRequests.js
+++ src/main/User/Login/authRequests.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import * as TYPES from '../../storage/actions'
 
 export const validateLogin = (dispatch, history, email, password) => {
-  console.log('email: ', email)
   const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL
   axios
     .post(`${GATEWAY_URL}/api/login`, null, {
@@ -12,14 +11,12 @@ export const validateLogin = (dispatch, history, email, password) => {
       },
     })
     .then(response => {
-      console.log('response: ', response)
       dispatch({
         type: TYPES.LOGIN,
       })
       history.push('/')
     })
     .catch(error => {
-      console.log('FAIL: Failed to login with error', error.message)
       dispatch({
         type: TYPES.SET_ERROR_MESSAGE,
         payload: {
