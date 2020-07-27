a5ff6e2cf2b39b546e7e330600c54728dc8a4943
sasha <sjlarson92@gmail.com>
2020-07-27 18:06:26 -0500

    handling response

--- src/__tests__/Image/imageApi.test.js
+++ src/__tests__/Image/imageApi.test.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { when } from 'jest-when'
 import { getImagesAction } from '../../main/Image/imageApi'
 import * as TYPES from '../../main/storage/actions'
 
@@ -24,25 +25,29 @@ describe('getImagesAction', () => {
     )
   })
 
-  // describe('when api response is resolved', () => {
-  //   it('should call dispatch with corret params', async () => {
-  //     axios.get.mockResolvedValue({
-  //       data: 'some image object',
-  //     })
-  //     await getImagesAction()(dispatch)
-  //     expect(dispatch).toHaveBeenCalledWith({
-  //       type: TYPES.SET_INITIAL_IMAGES,
-  //       payload: {
-  //         image: 'some image object',
-  //       },
-  //     })
-  //   })
-  // })
-  // describe('when api response is rejected', () => {
-  //   it('should not call dispatch', async () => {
-  //     axios.get.mockRejectedValue()
-  //     await getImagesAction()(dispatch)
-  //     expect(dispatch).not.toHaveBeenCalled()
-  //   })
-  // })
+  describe('when api response is resolved', () => {
+    it('should call dispatch with correct params', async () => {
+      const images = ['image1', 'image2']
+      const id = 1
+      when(axios.get)
+        .calledWith(`${GATEWAY_URL}/api/users/${id}/images`)
+        .mockResolvedValue({
+          data: images,
+        })
+      await getImagesAction(id)(dispatch)
+      expect(dispatch).toHaveBeenCalledWith({
+        type: TYPES.SET_INITIAL_IMAGES,
+        payload: {
+          image: images,
+        },
+      })
+    })
+  })
+  describe('when api response is rejected', () => {
+    it('should not call dispatch', async () => {
+      axios.get.mockRejectedValue()
+      await getImagesAction(1)(dispatch)
+      expect(dispatch).not.toHaveBeenCalled()
+    })
+  })
 })
--- src/main/Image/imageApi.js
+++ src/main/Image/imageApi.js
@@ -1,23 +1,15 @@
 import axios from 'axios'
-import _ from 'lodash'
 import * as TYPES from '../storage/actions'
-
-// export const getImagesAction = () => dispatch => {
-//   const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL
-//   _.times(3, () =>
-//     axios.get(`${GATEWAY_URL}/api/images/1`).then(response => {
-//       dispatch({
-//         type: TYPES.SET_INITIAL_IMAGES,
-//         payload: {
-//           image: response.data,
-//         },
-//       })
-//     }),
-//   )
-// }
 
 export const getImagesAction = id => dispatch => {
   const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL
   console.log(`making request with id: ${id}`)
-  axios.get(`${GATEWAY_URL}/api/users/${id}/images`)
+  axios.get(`${GATEWAY_URL}/api/users/${id}/images`).then(response => {
+    dispatch({
+      type: TYPES.SET_INITIAL_IMAGES,
+      payload: {
+        image: response.data,
+      },
+    })
+  })
 }
