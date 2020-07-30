9cf1f34fc8236c71af79202ac1683ca31d28e2f2
sasha <sjlarson92@gmail.com>
2020-07-29 20:17:29 -0500

    removed console log

--- src/main/Image/imageApi.js
+++ src/main/Image/imageApi.js
@@ -3,7 +3,6 @@ import * as TYPES from '../storage/actions'
 
 export const getImagesAction = id => dispatch => {
   const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL
-  console.log(`making request with id: ${id}`)
   axios.get(`${GATEWAY_URL}/api/users/${id}/images`).then(response => {
     dispatch({
       type: TYPES.SET_USER_IMAGES,
