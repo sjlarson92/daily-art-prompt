1c7db8a5467906d8b557bfe9b8acd8e6e14e4c02
sasha <sjlarson92@gmail.com>
2021-01-31 14:18:06 -0600

    add logo to navbar

--- src/main/Home/DapNavBar.js
+++ src/main/Home/DapNavBar.js
@@ -4,14 +4,15 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import * as TYPES from '../storage/actions'
+import logo from '../images/dapLogo.png'
 
 const DapNavBar = () => {
   const user = useSelector(state => state.user)
   const dispatch = useDispatch()
   const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL
   return (
-    <Navbar fixed="top" expand="sm" variant="dark" bg="dark">
-      <Navbar.Brand>Daily Art Prompt</Navbar.Brand>
+    <Navbar id="dap-navbar" fixed="top" expand="sm">
+      <img id="dap-logo" alt="dap logo" src={logo} />
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse className="justify-content-end">
         <NavDropdown id="nav-user-dropdown" alignRight title={user?.email}>
