3f0c8492c4d617e583bd905d1e341d670bdb1aac
sasha <sjlarson92@gmail.com>
2021-02-23 22:29:42 -0600

    edit comment and refactor

--- src/main/Comment/CommentLayout.js
+++ src/main/Comment/CommentLayout.js
@@ -2,49 +2,43 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencilAlt, faTimes } from '@fortawesome/free-solid-svg-icons'
 
-import Comment from './Comment'
-
-const CommentLayout = ({ comment, onDelete, onCancel, onEdit, onSubmit }) => (
-  <div>
-    {!comment.editing ? (
-      <div className="comment-layout-container" data-testid="nonEditingDiv">
-        <Comment comment={comment.text} />
-        <div id="comment-icon-container">
-          <FontAwesomeIcon
-            id="delete-icon"
-            className="pointer-on-hover"
-            icon={faTimes}
-            testid="deleteButton"
-            onClick={onDelete}
-          />
-          <FontAwesomeIcon
-            id="edit-icon"
-            testid="editButton"
-            onClick={onEdit}
-            icon={faPencilAlt}
-          />
-        </div>
-      </div>
-    ) : (
-      <div className="comment-layout-container" data-testid="editingDiv">
-        <input
-          className="comment-input-box"
-          testid="editInputBox"
-          defaultValue={comment.text}
-          onKeyDown={onSubmit}
-        />
-        <button
-          id="cancel-button"
-          className="btn"
-          type="button"
-          testid="cancelButton"
-          onClick={onCancel}
-        >
-          Cancel
-        </button>
-      </div>
-    )}
+export const Comment = ({ comment, onDelete, onEdit }) => (
+  <div className="comment-layout-container" data-testid="nonEditingDiv">
+    <div data-testid="commentDiv">{comment.text}</div>
+    <div id="comment-icon-container">
+      <FontAwesomeIcon
+        id="delete-icon"
+        className="pointer-on-hover"
+        icon={faTimes}
+        testid="deleteButton"
+        onClick={() => onDelete(comment.id)}
+      />
+      <FontAwesomeIcon
+        id="edit-icon"
+        testid="editButton"
+        onClick={onEdit}
+        icon={faPencilAlt}
+      />
+    </div>
   </div>
 )
 
-export default CommentLayout
+export const EditComment = ({ comment, onUpdate, onCancel }) => (
+  <div className="comment-layout-container" data-testid="editingDiv">
+    <input
+      className="comment-input-box"
+      testid="editInputBox"
+      defaultValue={comment.text}
+      onKeyDown={e => onUpdate(e, comment)}
+    />
+    <button
+      id="cancel-button"
+      className="btn"
+      type="button"
+      testid="cancelButton"
+      onClick={onCancel}
+    >
+      Cancel
+    </button>
+  </div>
+)
