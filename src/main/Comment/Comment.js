2664b55a36570611c31d1af81a5dfd194bd6e244
sasha <sjlarson92@gmail.com>
2021-02-25 18:55:03 -0600

    remove data-testids

--- src/main/Comment/Comment.js
+++ src/main/Comment/Comment.js
@@ -3,8 +3,8 @@ import { faPencilAlt, faTimes } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 const Comment = ({ comment, onDelete, onEdit }) => (
-  <div className="comment-layout-container" data-testid="nonEditingDiv">
-    <div data-testid="commentDiv">{comment.text}</div>
+  <div className="comment-layout-container">
+    <div>{comment.text}</div>
     <div id="comment-icon-container">
       <FontAwesomeIcon
         id="delete-icon"
