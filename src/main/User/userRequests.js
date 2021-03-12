c095b9bdccb8fd848803c7e12e42d0df72ce3b66
sasha <sjlarson92@gmail.com>
2021-03-11 23:02:05 -0600

    update api call to pass name in headers

--- src/main/User/userRequests.js
+++ src/main/User/userRequests.js
@@ -3,9 +3,10 @@ import { GATEWAY_URL } from '../constants'
 
 export const createUser = (dispatch, history, email, name, password) => {
   return axios
-    .post(`${GATEWAY_URL}/api/users`, name, {
+    .post(`${GATEWAY_URL}/api/users`, null, {
       headers: {
         email,
+        name,
         password,
       },
     })
