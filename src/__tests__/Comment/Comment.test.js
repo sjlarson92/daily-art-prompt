ad494b36a24dc7d96bbb4ce229a5ab3b540336f4
sasha <sjlarson92@gmail.com>
2021-03-14 19:36:08 -0500

    only show edit/delete for comments written by loggedIn user

--- src/__tests__/Comment/Comment.test.js
+++ src/__tests__/Comment/Comment.test.js
@@ -1,25 +1,59 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Comment from '../../main/Comment/Comment'
 
+jest.mock('react-redux')
+
+const user = {
+  id: 2,
+  email: 'SomeUser',
+}
 const defaultProps = {
-  comment: { id: 1, user: { id: 2 } },
+  comment: { id: 1, user },
   onDelete: jest.fn(),
   onEdit: jest.fn(),
 }
+const mockState = {
+  user,
+}
+
+const mockState2 = {
+  user: {
+    id: 7,
+  },
+}
 describe('<Comment />', () => {
-  describe('Delete Icon', () => {
-    it('call onDelete with correct params when clicked', () => {
+  describe('when comment.user.id is equal to userid', () => {
+    it('renders comment-icon-container', () => {
+      useSelector.mockImplementation(callback => callback(mockState))
       const wrapper = shallow(<Comment {...defaultProps} />)
-      wrapper.find({ testid: 'deleteButton' }).simulate('click')
-      expect(defaultProps.onDelete).toHaveBeenCalledWith(
-        defaultProps.comment.id,
-      )
+      expect(wrapper.find({ testid: 'iconContainer' })).toHaveLength(1)
+    })
+    describe('Delete Icon', () => {
+      it('call onDelete with correct params when clicked', () => {
+        jest.clearAllMocks()
+        useSelector.mockImplementation(callback => callback(mockState))
+        const wrapper = shallow(<Comment {...defaultProps} />)
+        wrapper.find({ testid: 'deleteButton' }).simulate('click')
+        expect(defaultProps.onDelete).toHaveBeenCalledWith(
+          defaultProps.comment.id,
+        )
+      })
+    })
+    describe('Edit Icon', () => {
+      jest.clearAllMocks()
+      useSelector.mockImplementation(callback => callback(mockState))
+      const wrapper = shallow(<Comment {...defaultProps} />)
+      wrapper.find({ testid: 'editButton' }).simulate('click')
+      expect(defaultProps.onEdit).toHaveBeenCalled()
     })
   })
-  describe('Edit Icon', () => {
-    const wrapper = shallow(<Comment {...defaultProps} />)
-    wrapper.find({ testid: 'editButton' }).simulate('click')
-    expect(defaultProps.onEdit).toHaveBeenCalled()
+  describe('when comment.user.id is NOT equal to userid', () => {
+    it('does NOT render comment-icon-container', () => {
+      useSelector.mockImplementation(callback => callback(mockState2))
+      const wrapper = shallow(<Comment {...defaultProps} />)
+      expect(wrapper.find({ testid: 'iconContainer' })).toHaveLength(0)
+    })
   })
 })
--- src/main/Comment/Comment.js
+++ src/main/Comment/Comment.js
@@ -1,28 +1,34 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencilAlt, faTimes } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-const Comment = ({ comment, onDelete, onEdit }) => (
-  <div className="comment-layout-container">
-    <div>
-      <strong>{comment.user.name}</strong> {comment.text}
+const Comment = ({ comment, onDelete, onEdit }) => {
+  const userId = useSelector(state => state.user.id)
+  return (
+    <div className="comment-layout-container">
+      <div>
+        <strong>{comment.user.name}</strong> {comment.text}
+      </div>
+      {comment.user.id === userId && (
+        <div testid="iconContainer" id="comment-icon-container">
+          <FontAwesomeIcon
+            id="delete-icon"
+            className="pointer-on-hover"
+            icon={faTimes}
+            testid="deleteButton"
+            onClick={() => onDelete(comment.id)}
+          />
+          <FontAwesomeIcon
+            id="edit-icon"
+            testid="editButton"
+            onClick={onEdit}
+            icon={faPencilAlt}
+          />
+        </div>
+      )}
     </div>
-    <div id="comment-icon-container">
-      <FontAwesomeIcon
-        id="delete-icon"
-        className="pointer-on-hover"
-        icon={faTimes}
-        testid="deleteButton"
-        onClick={() => onDelete(comment.id)}
-      />
-      <FontAwesomeIcon
-        id="edit-icon"
-        testid="editButton"
-        onClick={onEdit}
-        icon={faPencilAlt}
-      />
-    </div>
-  </div>
-)
+  )
+}
 
 export default Comment
